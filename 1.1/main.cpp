20f3b499e974fbd5e7b60836db5f72bb21ffbb43
zhangrun <1139135655@qq.com>
2023-02-12 22:40:56 +0800

    NEW: 1.1 files
    Signed-off-by: zhangrun <1139135655@qq.com>

---
+++ 1.1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string username;
+    cout << "input";
+    cin >> username;
+    cout << username << "hello" << endl;
+
+    return 0;
+}
